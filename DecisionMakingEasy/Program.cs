b58bde322b4dbadfce43da2d0beb723564f400c7
Akshay <akshaysrikumar@gmail.com>
2019-02-27 07:26:47 -0600

    c#

---
+++ DecisionMakingEasy/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DecisionMakingEasy
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            IfElse();
+            NestedIF();
+        }
+
+        private static void NestedIF()
+        {
+            int i = 10;
+
+            if (i > 0)
+            {
+                if (i <= 100)
+                {
+                    Console.WriteLine("i is positive number less than 100");
+                }
+                else
+                {
+                    Console.WriteLine("i is positive number greater than 100");
+                }
+            }
+        }
+
+        private static void IfElse()
+        {
+            int i = 10, j = 20;
+
+            if (i > j)
+                Console.WriteLine("i is greater than j");
+            else if (i < j)
+                Console.WriteLine("i is less than j");
+            else if (i == j)
+                Console.WriteLine("i is equal to j");
+        }
+    }
+}
---
+++ DecisionMakingHard/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace DecisionMakingHard
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            GotoSwitch();
+            NestedSwitch();
+        }
+
+        private static void NestedSwitch()
+        {
+            int j = 5;
+
+            switch (j)
+            {
+                case 5:
+                    Console.WriteLine(5);
+                    switch (j - 1)
+                    {
+                        case 4:
+                            Console.WriteLine(4);
+                            switch (j - 2)
+                            {
+                                case 3:
+                                    Console.WriteLine(3);
+                                    break;
+                            }
+                            break;
+                    }
+                    break;
+                case 10:
+                    Console.WriteLine(10);
+                    break;
+                case 15:
+                    Console.WriteLine(15);
+                    break;
+                default:
+                    Console.WriteLine(100);
+                    break;
+            }
+        }
+
+        private static void GotoSwitch()
+        {
+            string statementType = "switch";
+
+            switch (statementType)
+            {
+                case "DecisionMaking":
+                    Console.Write(" is a decision making statement.");
+                    break;
+                case "if.else":
+                    Console.Write("if-else");
+                    break;
+                case "ternary":
+                    Console.Write("Ternary operator");
+                    break;
+                case "switch":
+                    Console.Write("switch statement");
+                    goto case "DecisionMaking";
+            }
+        }
+    }
+}
